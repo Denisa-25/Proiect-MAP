2fad8c4d26b887fcb3f1f1b39c15196c8092956a
Denisa-25 <coconetudenisa2005@gmail.com>
2025-11-20 13:11:50 +0200

    Update cod.py
    prima data am incercat sa vad cum pun codul,iar acum am continuat putin cu variabilele de care am nevoie.

--- cod.py
+++ cod.py
@@ -1,2 +1,14 @@
 import heapq
 import random
+from dataclasses import dataclass, field
+from typing import Dict, List, Optional
+
+@dataclass(order=True)
+class Event:
+    time:float
+    priority: int
+    type: str= field(compare=False)
+    customer_id: int= field(compare=False)
+    server_id: Optional[int]= field(default=None, compare=False)
+    
+
